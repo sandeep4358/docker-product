584dd0e8ee6a8802b100f4ef12f11c635401a675
sandeep4358 <sandeep4358@gmail.com>
2024-02-02 11:43:59 +0530

    this change is for teh adding the project in the service discovery

--- src/main/java/com/javafruit/DockerComposePractice/DockerComposePracticeApplication.java
+++ src/main/java/com/javafruit/DockerComposePractice/DockerComposePracticeApplication.java
@@ -2,8 +2,10 @@ package com.javafruit.DockerComposePractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DockerComposePracticeApplication {
 
 	public static void main(String[] args) {
